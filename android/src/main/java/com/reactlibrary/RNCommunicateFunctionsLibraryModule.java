61285d7c9dcd410df04d90ad19aa9b442b9a1eee
Sanchit Manoj Wakekar <sanchit.wakekar@synapse.com>
2019-08-23 11:30:09 +0530

    Completed

--- android/src/main/java/com/reactlibrary/RNCommunicateFunctionsLibraryModule.java
+++ android/src/main/java/com/reactlibrary/RNCommunicateFunctionsLibraryModule.java
@@ -5,6 +5,9 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.*;
 
 public class RNCommunicateFunctionsLibraryModule extends ReactContextBaseJavaModule {
 
@@ -24,4 +27,29 @@ public class RNCommunicateFunctionsLibraryModule extends ReactContextBaseJavaMod
   public void show(String message) {
     message = "hello" + message;   
   }
+
+  @ReactMethod
+  public void CallbackShow(int number1, int number2,Callback successCallback) {
+    try {      
+      successCallback.invoke((number1 * number2));
+    } catch (Exception e) {
+      
+    }
+  }
+  @ReactMethod
+  public void PromiseShow(int number1,Callback successCallback) {
+    try {      
+      for(int i=0i<100;i++)
+      {
+        int Random = (int)(Math.random()*100);
+        if (Random == number1)
+        {
+           promise.resolve('Matched');
+        }
+     }      
+    } catch (Exception e) {
+       
+    }
+  }
+
 }
